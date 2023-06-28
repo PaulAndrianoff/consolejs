870dbdbe0281d0b8161a0edceb4a53cd6494552b
Paul Andrianoff <pandrianoff@gmail.com>
2023-06-28 11:08:19 +0200

    Update generate_scripts.php

--- generate_scripts.php
+++ generate_scripts.php
@@ -1,7 +1,6 @@
 <?php
-$directory = './scripts/commands'; // Specify the directory path where the command files are located
-
-$files = scandir($directory); // Get the list of files in the directory
+$files = scandir('./scripts/commands'); // Get the list of files in the directory
+$files = scandir('./scripts/libraries'); // Get the list of files in the directory
 
 foreach ($files as $file) {
     if (pathinfo($file, PATHINFO_EXTENSION) === 'js') {
